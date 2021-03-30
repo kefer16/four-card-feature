aa607953adf02b976bea621a4d5437f6c10d147b
kefer16 <morafi.1999.16@gmail.com>
2021-03-30 13:10:16 -0500

    animaciones

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", fnAnimations);
+
+
+function fnAnimations(){
+    
+    let texts = document.getElementById("texts");
+    let cards = document.getElementById("cards");
+
+    texts.classList.add("animation-on");
+    cards.classList.add("animation-on");
+    console.log("hola");
+}
